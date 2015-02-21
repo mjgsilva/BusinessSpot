3d9315a6232830ef7e303e81f0a73dd9ff0346c7
Ricardo Pereira <m@ricardopereira.eu>
2015-02-21 23:11:11 +0000

    Public Tenders

--- client/app/publictender/publictender.controller.js
+++ client/app/publictender/publictender.controller.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('businessSpotApp')
-  .controller('PublictenderCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('PublictenderCtrl', function ($scope, $http, $stateParams) {
+    $scope.tender = {};
+    $scope.currentId = $stateParams.id;
+
+    $http.get('/api/publictenders/' + $scope.currentId).success(function(tender) {
+      $scope.tender = tender;
+    });
   });
--- client/app/publictender/publictender.js
+++ client/app/publictender/publictender.js
@@ -4,9 +4,9 @@ angular.module('businessSpotApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('publictender', {
-        url: '/publictender',
+        url: '/publictender/{id:[0-9a-zA-Z-_]*}',
         templateUrl: 'app/publictender/publictender.html',
         controller: 'PublictenderCtrl',
         authenticate: true
-      });
+      })
   });
